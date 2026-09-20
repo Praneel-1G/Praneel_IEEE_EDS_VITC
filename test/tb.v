07344936d088908eee06dbeb0d0b091f7f939af5
Praneel.G <gundra.praneel@gmail.com>
2026-09-20 20:19:18 +0530

    Rename module instance from tt_um_protocol_nexus

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_protocol_nexus user_project (
+  tt_um_approx_mac_coprocessor  user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
